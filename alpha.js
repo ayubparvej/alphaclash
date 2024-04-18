9f45adf9a44b7e46de8ed6578db36c0e90d784aa
ayubparvej <ayub.pervej1@gmail.com>
2024-04-18 20:16:49 +0600

    design done

---
+++ alpha.js
@@ -0,0 +1,13 @@
+function play() {
+    //step-1: hide the home screen. to hide add hidden class in home section and add remove hidden class from artboard section.
+
+    const homeSection = document.getElementById('home-section');
+    homeSection.classList.add('hidden');
+
+    //step-2: show the playground
+    const playgroundSection = document.getElementById('playground-section');
+    playgroundSection.classList.remove('hidden');
+
+
+
+}
